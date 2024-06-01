1dee572f1fb7f20df5191d1551b487d49307cc26
johannonymous123 <77758164+johannonymous123@users.noreply.github.com>
2024-06-01 16:07:57 -0400

    Add files via upload
    Code decks for paperruns

---
+++ paper_runs/Hohlraum/Hybrid/Nx_52/S_8/input_hybrid.m
@@ -0,0 +1,19 @@
+%%Input file for Hybrid-runs
+%This file allows the user to change the user-defined parameters used in a
+%Hybrid run and starts the run afterwards.
+
+
+mode = 'hohlraum'; %Chose test problem. Options: hohlraum, hohlraum,lattice
+
+% Following inputs can be list and all combinations  of them will be used.
+n_cell = [52];   %number of spatial cells along each dimension 
+N_Qs = [1e4 1e5 1e6]; % number of new MC particles per MC step (including relabling)
+w_mins = [1e-15 ];  %killing weight
+n_mus = [8];        %number of ordinates for collided equation
+
+
+%Starts hybrid run
+cwd = pwd;
+cd ../../../../../
+run_hybrid;
+cd(cwd)
